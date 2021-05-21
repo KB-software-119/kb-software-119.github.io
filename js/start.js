400945bd0d4fe6c15c2066e5291f3e3f2f9982b9
Song-EunJu <jd06280@naver.com>
2021-05-21 18:49:58 +0900

    교통검사 결과작성

--- js/start.js
+++ js/start.js
@@ -35,88 +35,72 @@ function calTraffic(){
   var tree = 3.8;
   var num = select[0]; // 몇 번 선지
   var freq = qnaList[0].a[select[0]].data[0]; // 빈도
-  var result = Math.round(freq * co2 * 100)/100;
-  var total_tree = Math.round(freq * tree * 100)/100;
-  
-  if(num==0)
-    console.log("현재 가까운 거리를 도보나 자전거로 이용하고 있지 않습니다.\
-    주 1회라도 도보나 자전거를 이용한다면 연간 %fkg의 Co2를 저감하며 %f그루의 나무를 아낄 수 있습니다", co2, tree);
-  else if(num>=1 && num<=3)
-    console.log("현재 주 %d번 이상 %d번 미만 가까운 거리를 도보나 자전거로 이용하고 있습니다.\
-    연간 평균 %fkg의 Co2를 저감하며 %f그루의 나무를 아끼고 있습니다", freq-1, freq+1, result, total_tree);
-  else 
-    console.log("현재 주 %d번 이상 가까운 거리를 도보나 자전거로 이용하고 있습니다.\
-    연간 최소 %fkg의 Co2를 저감하며 %f그루의 나무를 아끼고 있습니다", freq, result, total_tree);
+  var total_co21 = Math.round(freq * co2 * 10)/10;
+  var total_tree1 = Math.round(freq * tree * 10)/10;
+
 
   // 2-1, 2-2번 문항 : 자동차 연료 종류와 연료비
-
+  var op = select[1]; // 자동차 이용 여부
   var fuel = select[2]; // 연료 선지
   var cost = qnaList[3].a[select[3]].data[0]; // 연료비
   var num = select[3]; // 연료비 선지
+  var total_co22;
+  var total_tree2;
+
+  if(op==0){
+    if(fuel==0){ // 휘발유
+      // 10000원 기준 co2 발생량과 필요 소나무
+      var base_co2 = 11.8; 
+      var base_tree = 1.8; 
+      total_co22 = Math.round(base_co2 + 11.7 * (cost-1) * 10)/10;
+      total_tree2 = Math.round(base_tree + 1.8 * (cost-1) * 10)/10;
+    }  
+    else if(fuel==1){ // 경유
+      var base_co2 = 16.2; 
+      var base_tree = 2.5; 
+      total_co22 = Math.round(base_co2 + 16.1 * (cost-1) * 10)/10;
+      total_tree2 = Math.round(base_tree + 2.4 * (cost-1) * 10)/10;
+    }  
+    else if(fuel==2){ // LPG
+      var base_co2 = 27.9; 
+      var base_tree = 4.2; 
+      total_co22 = Math.round(base_co2 + 27.8 * (cost-1) * 10)/10;
+      total_tree2 = Math.round(base_tree + 4.2 * (cost-1) * 10)/10;
+    }
+  }
+  else{
+      total_co22 = 0;
+      total_tree2 = 0;
+  }
   
-  if(fuel==0){ // 휘발유
-    // 10000원 기준 co2 발생량과 필요 소나무
-    var base_co2 = 11.8; 
-    var base_tree = 1.8; 
-    var total_co2 = Math.round(base_co2 + 11.7 * (cost-1) * 100)/100;
-    var total_tree = Math.round(base_tree + 1.8 * (cost-1) * 100)/100;
-    if(num==0)
-      console.log("현재 최대 %d만원 미만의 연료비를 지출하고 있습니다.\
-      월간 최대 %fkg의 Co2를 발생하고 있으며 이를 상쇄시키려면 %f그루의 나무가 필요합니다", cost, total_co2, total_tree);
-    else if(num>=1 && num<=3)
-      console.log("현재 %d만원 이상 %d만원 미만 연료비를 지출하고 있습니다.\
-      월간 평균 %fkg의 Co2를 발생하고 있으며 이를 상쇄시키려면 %f그루의 나무가 필요합니다", cost-2.5, cost+2.5, total_tree);
-    else
-      console.log("현재 최소 %d만원 이상 연료비를 지출하고 있습니다.\
-      월간 최소 %fkg의 Co2를 발생하고 있으며 이를 상쇄시키려면 %f그루의 나무가 필요합니다", cost, total_co2, total_tree);
-  }  
-  else if(fuel==1){ // 경유
-    var base_co2 = 16.2; 
-    var base_tree = 2.5; 
-    var total_co2 = Math.round(base_co2 + 16.1 * (cost-1) * 100)/100;
-    var total_tree = Math.round(base_tree + 2.4 * (cost-1) * 100)/100;
-    if(num==0)
-      console.log("현재 최대 %d만원 미만의 연료비를 지출하고 있습니다.\
-      월간 최대 %fkg의 Co2를 발생하고 있으며 이를 상쇄시키려면 %f그루의 나무가 필요합니다", cost, total_co2, total_tree);
-    else if(num>=1 && num<=3)
-      console.log("현재 %d만원 이상 %d만원 미만 연료비를 지출하고 있습니다.\
-      월간 평균 %fkg의 Co2를 발생하고 있으며 이를 상쇄시키려면 %f그루의 나무가 필요합니다", cost-2.5, cost+2.5, total_co2, total_tree);
-    else
-      console.log("현재 최소 %d만원 이상 연료비를 지출하고 있습니다.\
-      월간 최소 %fkg의 Co2를 발생하고 있으며 이를 상쇄시키려면 %f그루의 나무가 필요합니다", cost, total_co2, total_tree);
-  }  
-  else if(fuel==2){ // LPG
-    var base_co2 = 27.9; 
-    var base_tree = 4.2; 
-    var total_co2 = Math.round(base_co2 + 27.8 * (cost-1) * 100)/100;
-    var total_tree = Math.round(base_tree + 4.2 * (cost-1) * 100)/100;
-    if(num==0)
-      console.log("현재 최대 %d만원 미만의 연료비를 지출하고 있습니다.\
-      월간 최대 %fkg의 Co2를 발생하고 있으며 이를 상쇄시키려면 %f그루의 나무가 필요합니다", cost, total_co2, total_tree);
-    else if(num>=1 && num<=3)
-      console.log("현재 %d만원 이상 %d만원 미만 연료비를 지출하고 있습니다.\
-      월간 평균 %fkg의 Co2를 발생하고 있으며 이를 상쇄시키려면 %f그루의 나무가 필요합니다", cost-2.5, cost+2.5, total_co2, total_tree);
-    else
-      console.log("현재 최소 %d만원 이상 연료비를 지출하고 있습니다.\
-      월간 최소 %fkg의 Co2를 발생하고 있으며 이를 상쇄시키려면 %f그루의 나무가 필요합니다", cost, total_co2, total_tree);
-  }  
   // 2-3번 문항 : 대중교통 이용 횟수
   var co2 = 469.4;
   var tree = 71.1;
   var num = select[4]; // 몇 번 선지
   var freq = qnaList[4].a[select[4]].data[0]; // 빈도
-  var result = Math.round(freq * co2 * 100)/100;
-  var total_tree = Math.round(freq * tree * 100)/100;
+  var total_co23 = Math.round(freq * co2 * 10)/10;
+  var total_tree3 = Math.round(freq * tree * 10)/10;
   
-  if(num==0)
-    console.log("현재 승용차 대신 대중교통을 이용하고 있지 않습니다.\
-    주 1회라도 대중교통을 이용한다면 연간 %fkg의 Co2를 저감하며 %f그루의 나무를 아낄 수 있습니다", co2, tree);
-  else if(num==1)
-    console.log("현재 주 %d번 이상 %d번 미만 승용차 대신 대중교통을 이용하고 있습니다.\
-    연간 평균 %fkg의 Co2를 저감하며 %f그루의 나무를 아낄 수 있습니다", freq-2, freq+2, result, total_tree);
-  else 
-    console.log("현재 주 %d번 이상 %d번 미만 승용차 대신 대중교통을 이용하고 있습니다.\
-    연간 평균 %fkg의 Co2를 저감하며 %f그루의 나무를 아낄 수 있습니다", freq-2.5, freq+2.5, result, total_tree);
+  document.getElementById("i_ans1_co2").innerHTML=total_co21;
+  document.getElementById("i_ans1_tree").innerHTML=total_tree1;
+
+  // 자동차 운행으로 발생되는 값
+  document.getElementById("i_ans2_co2").innerHTML=total_co22;
+  document.getElementById("i_ans2_tree").innerHTML=total_tree2;
+
+  document.getElementById("i_ans3_co2").innerHTML=total_co23;
+  document.getElementById("i_ans3_tree").innerHTML=total_tree3;
+
+  traffic_co2 = Math.round((total_co22-(total_co21+total_co23)*10))/10;
+  traffic_tree = Math.round((total_tree2-(total_tree1+total_tree3)*10))/10;
+
+  
+  if(traffic_co2<0){
+    $('.resource').append(`<span>연간 CO2 절감량 : ${traffic_co2*(-1)} <br>연간 아끼고 있는 소나무 : ${traffic_tree*(-1)}그루</span>`);
+  }
+  else{
+    $('.resource').append(`<span>연간 CO2 배출량 : ${traffic_co2} <br>연간 필요한 소나무 : ${traffic_tree}그루</span>`);
+  }
 }
 
 
